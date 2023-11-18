9ccf99d8563ead518f78b521faf1a959dcd3a853
YtG12 <yitiang1@gmail.com>
2023-11-17 19:42:42 -0800

    create login screen view

---
+++ PawPal/PawPal/Login Screen/LoginScreenView.swift
@@ -0,0 +1,20 @@
+//
+//  LoginScreenView.swift
+//  PawPal
+//
+//  Created by Yitian Guo on 11/17/23.
+//
+
+import UIKit
+
+class LoginScreenView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
