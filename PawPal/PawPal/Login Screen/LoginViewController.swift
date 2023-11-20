0a6e04416f9e04df84e91b561f2ea2322fd05a29
YtG12 <151299641+YtG12@users.noreply.github.com>
2023-11-19 17:19:59 -0800

    Add files via upload

--- PawPal/PawPal/Login Screen/LoginViewController.swift
+++ PawPal/PawPal/Login Screen/LoginViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class LoginViewController: UIViewController {
     
@@ -26,7 +27,17 @@ class LoginViewController: UIViewController {
     }
     
     @objc func onLoginTapped() {
-        
+        //MARK: Log In Action...
+        if let email = loginView.textFieldEmail.text,
+           let password = loginView.textFieldPassword.text{
+            //check if the textfile is empty
+            if email.isEmpty || password.isEmpty{
+                self.showAlert(title: "Error!", message: "Please fill in all information!")
+            } else {
+                //MARK: sign-in logic for Firebase...
+                self.signInToFirebase(email: email, password: password)
+            }
+        }
     }
     
     @objc func onSignUpTapped() {
@@ -36,6 +47,37 @@ class LoginViewController: UIViewController {
     
     @objc func onForgotPasswordTapped() {
         
+    }
+    
+    func signInToFirebase(email: String, password: String){
+        //MARK: display progress indicator here
+        //showActivityIndicator()
+        
+        //MARK: authenticating the user...
+        Auth.auth().signIn(withEmail: email, password: password, completion: {(result, error) in
+            if error == nil{
+                //MARK: user authenticated...
+                //MARK: hide the progress indicator here
+                //self.hideActivityIndicator()
+                print("Login successful")
+                // Transition to the main tab bar controller
+                let mainTabBarController = ViewController()
+                mainTabBarController.modalPresentationStyle = .fullScreen
+                self.present(mainTabBarController, animated: true, completion: nil)
+            }else{
+                //MARK: alert that no user found or password wrong...
+                //MARK: hide the progress indicator here
+                //self.hideActivityIndicator()
+                self.showAlert(title: "Error!", message: "Incorrect user email or password")
+            }
+        })
+    }
+    
+    //MARK: Alert controller logics...
+    func showAlert(title: String, message: String){
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default))
+        self.present(alert, animated: true)
     }
 
 }
