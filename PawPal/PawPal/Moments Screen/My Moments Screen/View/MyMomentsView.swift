87d2ec32cd8d6aaf6d4bde26605939da00d2c29f
YtG12 <yitiang1@gmail.com>
2023-11-22 09:37:42 -0800

    Update post logic; Update UI

---
+++ PawPal/PawPal/Moments Screen/My Moments Screen/View/MyMomentsView.swift
@@ -0,0 +1,20 @@
+//
+//  MyMomentsView.swift
+//  PawPal
+//
+//  Created by Yitian Guo on 11/22/23.
+//
+
+import UIKit
+
+class MyMomentsView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
