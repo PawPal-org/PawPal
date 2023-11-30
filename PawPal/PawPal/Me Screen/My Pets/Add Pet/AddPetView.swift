cb38ec992737af616dc8d6bca78d214ef65a22fb
zzrcynthia <zzrcynthia@163.com>
2023-11-29 16:35:32 -0800

    Add Pet page view: card view look

---
+++ PawPal/PawPal/Me Screen/My Pets/Add Pet/AddPetView.swift
@@ -0,0 +1,20 @@
+//
+//  AddPetView.swift
+//  PawPal
+//
+//  Created by Cynthia Zhang on 11/29/23.
+//
+
+import UIKit
+
+class AddPetView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ PawPal/PawPal/Me Screen/My Pets/Add Pet/AddPetViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AddPetViewController.swift
+//  PawPal
+//
+//  Created by Cynthia Zhang on 11/29/23.
+//
+
+import UIKit
+
+class AddPetViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
